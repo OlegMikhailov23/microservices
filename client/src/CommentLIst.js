887f24237448520911af0995d002e415e9179c04
Oleg Mikhaylov <dkfolkin@gmail.com>
2023-05-14 18:32:43 +0300

    add queryService

--- client/src/CommentLIst.js
+++ client/src/CommentLIst.js
@@ -1,21 +1,7 @@
-import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-export const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `//localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const renderComments = comments.map((c) => {
+export const CommentList = ({ postComments }) => {
+  const renderComments = postComments?.map((c) => {
     return <li key={c.id}>{c.content}</li>;
   });
 
